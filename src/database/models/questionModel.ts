578271525ffc5c60125bfab54d9a6262eee53640
SkyLightQP <combbm@naver.com>
2020-09-25 12:52:28 +0900

    fix: apply auto increment id

--- src/database/models/questionModel.ts
+++ src/database/models/questionModel.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
+import autoIncrement from 'mongoose-auto-increment';
 
 export interface QuestionAttribute extends mongoose.Document {
-  readonly id: number;
-  readonly studyDataId: number;
-  readonly userId: string;
-  readonly title: string;
-  readonly content: string;
-  readonly like: number;
-  readonly slideOrder: number;
-  readonly slideImageURL: string;
-  readonly createdAt: Date;
-  readonly updatedAt: Date;
+    readonly id: number;
+    readonly studyDataId: number;
+    readonly userId: string;
+    readonly title: string;
+    readonly content: string;
+    readonly like: number;
+    readonly slideOrder: number;
+    readonly slideImageURL: string;
+    readonly createdAt: Date;
+    readonly updatedAt: Date;
 }
 
 export const questionSchema: mongoose.Schema = new mongoose.Schema({
@@ -57,6 +58,13 @@ export const questionSchema: mongoose.Schema = new mongoose.Schema({
   }
 }, {
   timestamps: true
+});
+
+questionSchema.plugin(autoIncrement.plugin, {
+  model: 'question',
+  field: 'id',
+  startAt: 1,
+  increment: 1
 });
 
 const questionModel = mongoose.model<QuestionAttribute>('question', questionSchema);
