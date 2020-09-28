9a7f648bd724329c9bc86a60565502de652e120c
combbm <combbm@naver.com>
2020-09-28 16:40:27 +0900

    fix: apply encodeURI() function

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,10 +4,8 @@ import config from '../config';
 const initDatabase = (): void => {
   const { username, password, host, database, port } = config.database;
 
-  console.log(`mongodb://${username}:${password}@${host}:${port}/${database}?authSource=admin&authMechanism=SCRAM-SHA-1`);
-
   mongoose.connect(
-    `mongodb://${username}:${password}@${host}:${port}/${database}?authSource=admin&authMechanism=SCRAM-SHA-1`,
+    encodeURI(`mongodb://${username}:${password}@${host}:${port}/${database}?authSource=admin&authMechanism=SCRAM-SHA-1`),
     {
       useNewUrlParser: true,
       useUnifiedTopology: true
